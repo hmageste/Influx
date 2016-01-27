b73a68cc9b3213ba9f6c01b6588eead153b954fc
hmageste <henrique.mageste@dgbes.com>
2016-01-27 02:53:00 -0200

    Declare some variables to allow calculation

--- cdata.h
+++ cdata.h
@@ -9,9 +9,31 @@ public:
 	    ~CData();
     void    ManualEntry();
     void    FileEntry();
+
 protected:
 
 private:
+    double re; ///< aquiver extern radius
+    double ro; ///< reservoir radius
+    double cf; ///< Formation compressibility
+    double cw; ///< Water compressibility
+    double h;  ///< Aquifer height
+    double f;  ///<
+    double k;  ///< Water permeability
+    double ko; ///< Oil permeability
+    double Mi; ///< Water viscosity
+    double pi; ///< Aquifer intern pressure
+    double po; ///< Aquifer extern pressure
+    double Wd; ///<
+    double t;  ///< Time
+    double phi;///< Aquifer porosity
+    double We;
+    double Wei;
+    double U;
+    double td;
+    double deltapo;
+    double red;
+    double J;
 };
 
 #endif // CDATA_H
