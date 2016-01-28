8123db20c630821d129d9a8b0ce5e72cf6b9c2b6
hmageste <henrique.mageste@dgbes.com>
2016-01-28 02:59:29 -0200

    Implement main menu

--- main.cpp
+++ main.cpp
@@ -1,20 +1,58 @@
 #include <iostream>
+#include <cstddef>
 #include "ccartertracy.h"
 #include "ceverdingenhurst.h"
 #include "cfetkovich.h"
 #include "sfluid.h"
+#include "cgnuplot.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-
-    CRadialAquifer aquifer;
-    CFetkovich fetkovich( &aquifer );
-    CCarterTracy cartertracy( &aquifer );
-    CEverdingenHurst everdingenHurst( &aquifer );
     SFluid water;
+    CRadialAquifer aquifer;
+    // fulfill aquifer information
+
+    CBaseClass* method = NULL;
+
+    int resp = 0;
+    do
+    {
+	cout << "\n\nDigite o método de resolução"
+	     << "\n1 - Everdingen & Hurst"
+	     << "\n2 - Fetkovich"
+	     << "\n3 - Carter-Tracy"
+	     << "\n0 - Sair"
+	     << "\nOpcao numero: ";
+	cin >> resp; cin.get();
+
+	switch( resp )
+	{
+	    case 1: method = new CEverdingenHurst( &aquifer );	break;
+	    case 2: method = new CFetkovich( &aquifer );	break;
+	    case 3: method = new CCarterTracy( &aquifer );	break;
+	    case 0: return 0;
+	    default: cerr << "\nModelo incorreto! Escolha um modelo válido!" << endl; break;
+	}
+
+	if ( method != NULL )
+	{
+	    method->calcWe();
+
+	    // Plotar com GnuPlot We x t
+	    /*
+	    CGnuplot gnuplot (x, y, "Pontos Observados", "points", "x", "y");
+
+	    gnuplot.Style("lines");
+	    gnuplot.PlotEquation( pobj->FuncaoAprox().erase(0,4) ); // plota função aproximadora sem
+								    // os 4 caracteres iniciais: "y = "
+	    */
+	    delete method;
+	    method = NULL;
+	}
+	getchar();
+    } while (resp != 0);
 
     return 0;
 }
