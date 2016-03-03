2edca5b787d59bd0d32f920c27e3d1e98702cce4
hmageste <hmageste@gmail.com>
2016-03-03 11:14:24 -0300

    Found mistake in the way factorial was calculated, n-- decreases the variable value

--- ceverdingenhurst.cpp
+++ ceverdingenhurst.cpp
@@ -94,7 +94,7 @@ int factorial( int n )
     if ( n < 2 )
 	return 1;
 
-    return n*factorial(--n);
+    return n*factorial(n-1);
 }
 
 // Numerical Inverse Laplace Transform using Gaver-Stehfest method
